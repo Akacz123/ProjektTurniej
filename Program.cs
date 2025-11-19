5463916b5fe4d049cc1ddfa286443d442640381a
Akacz123 <100wiktor@gmail.com>
2025-11-19 18:04:31 +0100

    Zrobienie API

--- Program.cs
+++ Program.cs
@@ -16,6 +16,15 @@ builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseNpgsql(connectionString));
 
 
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll", policy =>
+    {
+        policy.AllowAnyOrigin() 
+              .AllowAnyMethod()  
+              .AllowAnyHeader();
+    });
+});
 var app = builder.Build(); 
 
 if (app.Environment.IsDevelopment())
@@ -25,6 +34,8 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.UseCors("AllowAll");
 
 app.UseAuthorization();
 
