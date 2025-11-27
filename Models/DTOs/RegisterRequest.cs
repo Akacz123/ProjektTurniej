ae6b64edbb2e6f243666e6a00ad74e7eb38d4328
Milosz <mp134958@stud.ur.edu.pl>
2025-11-27 19:19:51 +0100

    test bozy

--- Models/DTOs/RegisterRequest.cs
+++ Models/DTOs/RegisterRequest.cs
@@ -5,7 +5,7 @@ namespace EsportsTournament.API.Models.DTOs
     public class RegisterRequest
     {
         [Required(ErrorMessage = "Nazwa użytkownika jest wymagana.")]
-        [StringLength(50, MinimumLength = 3, ErrorMessage = "Nazwa użytkownika musi mieć od 3 do 50 znaków.")]
+        
         public string Username { get; set; } = string.Empty;
 
         [Required(ErrorMessage = "Adres e-mail jest wymagany.")]
@@ -15,7 +15,6 @@ namespace EsportsTournament.API.Models.DTOs
         public string Email { get; set; } = string.Empty;
 
         [Required(ErrorMessage = "Hasło jest wymagane.")]
-        [StringLength(50, MinimumLength = 8, ErrorMessage = "Hasło musi mieć co najmniej 8 znaków.")]
         public string Password { get; set; } = string.Empty;
 
         [StringLength(50, ErrorMessage = "Imię nie może być dłuższe niż 50 znaków.")]
