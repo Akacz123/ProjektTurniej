9b0bbf319b12f8354efafff2be5ba2b5d68801d1
Akacz123 <100wiktor@gmail.com>
2026-01-03 14:32:47 +0100

    Stworzenie GamesController do wyświetlania listy gier wszystkich albo za pomocą id

---
+++ Controllers/GamesController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using EsportsTournament.API.Data;
+using EsportsTournament.API.Models;
+
+namespace EsportsTournament.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GamesController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public GamesController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Game>>> GetGames()
+        {
+            return await _context.Games.ToListAsync();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Game>> GetGame(int id)
+        {
+            var game = await _context.Games.FindAsync(id);
+
+            if (game == null)
+            {
+                return NotFound();
+            }
+
+            return game;
+        }
+    }
+}
